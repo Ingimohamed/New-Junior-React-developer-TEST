adca7ecab318a01e5b46101b01c43171604c6e0c
Ingi Mohamed <ingimohamed2222@gmail.com>
2021-06-18 11:39:42 +0200

    add sass library

--- shopping-react/src/App.js
+++ shopping-react/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar';
 import configureStore from './store/configureStore';
 import ClothesItems from './components/ClothesItems';
